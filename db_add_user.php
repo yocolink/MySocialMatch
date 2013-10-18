8da9b1f27842c7fd17d22fe42878060227ed4420
yocolink <lu.nitoumbi@gmail.com>
2013-10-18 12:45:55 +0200

    Avance dans le developpement front Correction db_add_user

--- db_add_user.php
+++ db_add_user.php
@@ -1,43 +1,42 @@
 <?php
-	include "connexion.php";
+	require_once "connexion.php";
+        $id = $_POST['id'];
+        $birthdate = date('Y-m-d', strtotime($_POST['birthdate']));
+
+        // Vérifier si l'id de l'user apparaît dans la table user
+        $query = "SELECT COUNT(*) FROM users WHERE id_user=$id";
+        $exec = mysqli_query($connect,$query) or die("<p>Erreur SQL : " . mysqli_error($connect) ."</p>");
+
+        $resultat = mysqli_fetch_row($exec);
+        $nblignes = $resultat[0];
+
+        // Si aucune ligne de trouvée alors
+        if($nblignes == 0){
+
+                // Ajout de l'utilisateur dans la table users
+                $query = "INSERT INTO users(id_user, user_bday, date_creation, last_connection) VALUES ($id, CAST('".$birthdate."' AS DATE),CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
+                $exec = mysqli_query($connect,$query) or die("Erreur SQL : " . mysqli_error($connect));
+                echo "Utilisateur ajouté avec succès !";
+        }
+        // Sinon
+        else{
+            // Vérifier si l'user a changé sa date de naissance
+            $query = "SELECT user_bday FROM users WHERE id_user=$id";
+            $exec = mysqli_query($connect,$query) or die("<p>Erreur SQL : " . mysqli_error($connect) ."</p>");
+
+            $resultat = mysqli_fetch_row($exec);
+            $current_user_bday = $resultat[0];
+
+            if ($current_user_bday != $birthdate)
+                    $current_user_bday = $birthdate;
+
+            // On update la date de naissance et de dernière connexion dans la table user
+            $query = "UPDATE users SET last_connection=CURRENT_TIMESTAMP, user_bday=$current_user_bday WHERE id_user=$id";
+            $exec = mysqli_query($connect,$query)  or die("Erreur SQL : " . mysqli_error($connect) );
+            echo "Dernière connexion mise à jour !";
+        }
+
+		
+        
+        mysqli_close($connect);
 ?>
-<!DOCTYPE HTML>
-<html>
-<head>
-	<title></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-</head>
-<body>
-	<?php
-		//$id = $_POST['id'];
-		//$birthdate = $_POST['birthdate'];
-		
-		$id = 151532;
-		$birthdate = "1991-01-21";
-		
-		// Vérifier si l'id de l'user apparaît dans la table user
-		$query = "SELECT COUNT(*) FROM users WHERE id_user=$id";
-		$exec = mysqli_query($connect,$query) or die("<p>Erreur SQL : " . mysqli_error($connect) ."</p>");
-		
-		$resultat = mysqli_fetch_row($exec);
-		$nblignes = $resultat[0];
-		echo "<p>$nblignes lignes trouvées </p>";
-		
-		// Si aucune ligne de trouvée alors
-		if($nblignes == 0){
-		
-			// Ajout de l'utilisateur dans la table users
-			$query = "INSERT INTO users(id_user, user_bday, date_creation, last_connection) VALUES ($id, CAST('".$birthdate."' AS DATE),CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
-			$exec = mysqli_query($connect,$query) or die("<p>Erreur SQL : " . mysqli_error($connect) ."</p>");
-			echo "<p>Utilisateur ajouté avec succès !</p>";
-		}
-		// Sinon
-		else{
-			// On update la date de dernière connexion
-			$query = "UPDATE users SET last_connection=CURRENT_TIMESTAMP WHERE id_user=$id";
-			$exec = mysqli_query($connect,$query)  or die("<p>Erreur SQL : " . mysqli_error($connect) ."</p>");
-			echo "<p>Dernière connexion mise à jour !</p>";
-		}
-	?>
-</body>
-</html>
